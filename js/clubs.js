18c9ee39900495689553efdaa13691b495ca3970
Rizwan Hussain <rizwan.ah07@hotmail.com>
2024-06-27 16:18:35 +0000

    loaded images in league and club, pagination????

--- js/clubs.js
+++ js/clubs.js
@@ -14,12 +14,12 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 const User_ID = "4ef6b7b4-a42d-4856-b5fb-18c57ec6f107";
-const base_api_url = "https://futdb.app/api/leagues";
-let leagues = [];
+const base_api_url = "https://futdb.app/api/clubs";
+let clubs = [];
 let pageCurrent = 1;
 const pageTotal = 4; // total number of pages
 
-function fetchLeagues(page) {
+function fetchClubs(page) {
     return fetch(`${base_api_url}?page=${page}`, {
         method: 'GET',
         headers: {
@@ -34,21 +34,21 @@ function fetchLeagues(page) {
         return response.json();
     })
     .then(data => {
-        leagues = leagues.concat(data.items);
+        clubs = clubs.concat(data.items);
     })
     .catch(error => {
         console.error('There was a problem with fetching the data', error);
     });
 }
 
-async function fetchAllLeagues() {
+async function fetchAllClubs() {
     for (let page = 1; page <= pageTotal; page++) {
-        await fetchLeagues(page);
+        await fetchClubs(page);
     }
 
-    const leaguesContainer = document.getElementById('leagues');
-    leagues.filter(league => !league.name.includes('WC')).filter(league => league.id !== 2179).forEach(league => {
-        const img_url = `https://futdb.app/api/leagues/${league.id}/image`;
+    const clubsContainer = document.getElementById('clubs');
+    clubs.filter(club => club.id !== 60).forEach(club => {
+        const img_url = `https://futdb.app/api/clubs/${club.id}/image`;
 
         fetch(img_url, {
             method: 'GET',
@@ -65,22 +65,22 @@ async function fetchAllLeagues() {
         })
         .then(blob => {
             const url = URL.createObjectURL(blob);
-            leaguesContainer.innerHTML += `
+            clubsContainer.innerHTML += `
                 <article>
-                    <img src="${url}" alt="league logo" style="width: 150px; height: 150px;">
-                    <p>${league.name}</p>
+                    <img src="${url}" alt="club logo" style="width: 150px; height: 150px;">
+                    <p>${club.name}</p>
                 </article>
             `;
         })
         .catch(error => {
-            console.error(`There was a problem with fetching the image for league ${league.id}`, error);
-            leaguesContainer.innerHTML += `
+            console.error(`There was a problem with fetching the image for club ${club.id}`, error);
+            clubsContainer.innerHTML += `
                 <article>
-                    <p>${league.name} (Image not available)</p>
+                    <p>${club.name} (Image not available)</p>
                 </article>
             `;
         });
     });
 }
 
-fetchAllLeagues();
+fetchAllClubs();
